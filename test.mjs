d296e84c6c1ff8a369b127308a1394ab18a20f5c
softliner <79776235+softliner@users.noreply.github.com>
2024-11-03 23:03:07 +0100

    Update test.mjs
    Adjusted jito tips

--- test.mjs
+++ test.mjs
@@ -18,7 +18,7 @@ const mint = new PublicKey("YOUR_TOKEN_MINT")
 
 // makers
 const amm = new Amm(connection, payerWallet)
-await amm.makers(mint, 5000, {jitoTipLamports: 100001})
+await amm.makers(mint, 5000, {jitoTipLamports: 10001})
 
 
 // volume
@@ -26,4 +26,4 @@ const minSolPerSwap = 0.005 // sol per swap
 const maxSolPerSwap = 0.006 // sol per swap
 const mCapFactor = 1 // adjust this to make chart go up. higher number = higher chart, you just are left over with some tokens that you can sell after.
 const speedFactor = 1  // Adjust this value to control trading frequency
-await amm.volume(mint, minSolPerSwap, maxSolPerSwap, mCapFactor, speedFactor, {jitoTipLamports: 300001}) // higher mCap factor = chart goes up. 0 = no increase or decrease - net neutral.
+await amm.volume(mint, minSolPerSwap, maxSolPerSwap, mCapFactor, speedFactor, {jitoTipLamports: 30001}) // higher mCap factor = chart goes up. 0 = no increase or decrease - net neutral.
