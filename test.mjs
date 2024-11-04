3086643988a3296be63c608498b5b26721f0bffa
shadowystupidcoder <154037987+shadowystupidcoder@users.noreply.github.com>
2024-11-04 08:53:58 -0700

    Update test.mjs

--- test.mjs
+++ test.mjs
@@ -1,29 +1,23 @@
 import { Connection, Keypair, PublicKey } from "@solana/web3.js"
-import Amm from "./src/Amm.js"
+import Amm from "@confirmedwtf/solana-amm-sdk"
 import bs58 from "bs58"
 
-
 // setup
-const connection = new Connection("YOUR_RPC_URL")
-const payerWallet = Keypair.fromSecretKey(Uint8Array.from( [/* Your keypair */] ))
-// const jupFeeAddress = new PublicKey("YOUR_JUP_FEE_ADDRESS")
-// const feeBPS = 100 // 1%
-// jup referral program not yet fully implemented, check back later today or tomorrow.
-
+const connection = new Connection("https://lauraine-qytyxk-fast-mainnet.helius-rpc.com/")
+const payerWallet = Keypair.fromSecretKey(Uint8Array.from(/* Your keypair */))
 // if you need to use a phantom style base58 string private key:
 // const payerWallet = Keypair.fromSecretKey(bs58.decode(/* Your private key */))
+const mint = new PublicKey("Grass7B4RdKfBCjTKgSqnXkqjwiGvQyFbuSCUJr3XXjs")
 
-const mint = new PublicKey("YOUR_TOKEN_MINT")
-
-
-// makers
+// create amm instance
 const amm = new Amm(connection, payerWallet)
-await amm.makers(mint, 5000, {jitoTipLamports: 10001})
 
+// do makers
+await amm.makers(mint, 5000, { jitoTipLamports: 10000 })
 
-// volume
+// do volume
 const minSolPerSwap = 0.005 // sol per swap
 const maxSolPerSwap = 0.006 // sol per swap
 const mCapFactor = 1 // adjust this to make chart go up. higher number = higher chart, you just are left over with some tokens that you can sell after.
-const speedFactor = 1  // Adjust this value to control trading frequency
-await amm.volume(mint, minSolPerSwap, maxSolPerSwap, mCapFactor, speedFactor, {jitoTipLamports: 30001}) // higher mCap factor = chart goes up. 0 = no increase or decrease - net neutral.
+const speedFactor = 1  // adjust this value to control swap frequency
+await amm.volume(mint, minSolPerSwap, maxSolPerSwap, mCapFactor, speedFactor, {jitoTipLamports: 100000}) // Jito tip defaults to 10000 lamports if omitted
